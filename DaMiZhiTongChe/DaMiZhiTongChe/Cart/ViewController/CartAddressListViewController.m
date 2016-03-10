6dcd44a5111a9ece41fe1a45f4465f72266b8cfe
Xiaodong Jiang <jxdwinter@gmail.com>
2016-03-10 12:56:12 +0800

    udpate

--- DaMiZhiTongChe/DaMiZhiTongChe/Cart/ViewController/CartAddressListViewController.m
+++ DaMiZhiTongChe/DaMiZhiTongChe/Cart/ViewController/CartAddressListViewController.m
@@ -10,6 +10,8 @@
 
 @interface CartAddressListViewController ()
 
+@property (nonatomic, strong) UIButton *addButton;
+
 @end
 
 @implementation CartAddressListViewController
@@ -19,11 +21,29 @@
     // Do any additional setup after loading the view.
     self.backButton.hidden = NO;
     self.title = @"收货地址";
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.addButton];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - privete method
+
+- (void) addAddress {
+    
+}
+
+#pragma mark - getter and setter
+
+- (UIButton *) addButton {
+    if (!_addButton) {
+        _addButton = [[UIButton alloc] initWithFrame:CGRectMake(0.0, 0.0, 20.0, 20.0)];
+        [_addButton addTarget:self action:@selector(addAddress) forControlEvents:UIControlEventTouchUpInside];
+        [_addButton setImage:[UIImage imageNamed:@"cart_add"] forState:UIControlStateNormal];
+    }
+    return _addButton;
 }
 
 @end
